4b197b1111c204373f3033355229eee8e5a34b38
joshrubio <155823600+joshrubio@users.noreply.github.com>
2025-06-24 19:32:51 +0200

    BASE_URL added for deployment porpuses

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"; // Adjust the base URL based on the environment
 const api = axios.create({ 
 
-    baseURL: "http://localhost:5001/api",
+    baseURL: BASE_URL,
 })
 
 export default api;
