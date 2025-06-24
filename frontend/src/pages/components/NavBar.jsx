8608c8b36af20038ce4753bc2abf8bf8250d24af
joshrubio <155823600+joshrubio@users.noreply.github.com>
2025-06-24 19:43:49 +0200

    Changed name of the app

--- frontend/src/pages/components/NavBar.jsx
+++ frontend/src/pages/components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
     return <header className="bg-base-300 border-b border-base-content/10">
                 <div className="container mx-auto max-w-6xl p-4"> 
                     <div className="flex items-center justify-between"> 
-                       <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">Thinkspace</h1>
+                       <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">Thinkboard</h1>
                           <div className="flex items-center gap-4">
                                 <Link to={"/create"} className="btn btn-primary">
                                 <PlusIcon className="size-5"/>
