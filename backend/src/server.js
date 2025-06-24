fedaf4f699232d97d005e0a87498d762cb0e1548
joshrubio <155823600+joshrubio@users.noreply.github.com>
2025-06-24 19:11:53 +0200

    prepare for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import notesRoutes from './routes/notesRoutes.js'
 import { connectDB } from './config/db.js'
 import dotenv from 'dotenv'
+import path from 'path'
 import { Redis } from "@upstash/redis";
 import rateLimiter from './middleware/rateLimiter.js';
 dotenv.config();
@@ -11,11 +12,22 @@ dotenv.config();
 
 const app = express () 
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
+if (process.env.NODE_ENV !== 'production') {
 app.use (cors ({}))  // Enable CORS for all origins
+}
+
 app.use(express.json()) // Parse JSON bodies
 app.use(rateLimiter) // Apply rate limiting middleware
 app.use ("/api/notes", notesRoutes) // Use notes routes
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist"))) // Serve static files from the public directory
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html")) // Serve index.html for all other routes
+})
+}
 
 
 connectDB().then (() => {
